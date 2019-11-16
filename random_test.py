3c2e04b7c1942d404c3b40bee4896c9a31725538
IsabellaCuiCui <40281980+IsabellaCuiCui@users.noreply.github.com>
2019-11-16 20:38:36 +0800

    Result of program
    0.10871906943701137
    1.2537837766183952
    -10
    3.3
    e
    [5, 6, 1, 4.4, 2.2, 3.3]
    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
    [0, 3, 5, 8, 4, 6, 7, 9, 1, 2]
    [8, 0, 7, 2, 1]

---
+++ random_test.py
@@ -0,0 +1,30 @@
+# 用于测试random模块中各类函数的效果
+import random
+
+a = random.random() #用于生成一个随机的0~1之间的浮点数
+b = random.uniform(1, 5) # 用于生成一个有范围的浮点数
+c = random.randint(-11, -5) #用于生成一个有范围的整数
+
+d1 = [1 ,2.2, 3.3, 4.4, 5, 6]
+d = random.choice(d1) #用于从列表中选取元素返回
+
+e1 = "abcdefg"
+e = random.choice(e1) #用于从字符串中选取元素返回
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+random.shuffle(d1)
+print(d1) #注意print的不是rando.shuffle(d1),而是先调用函数后，再print变量d1，否则会返回None值
+#random.shuffle函数不支持对字符串的乱序，random.shuffle(e1)会报错
+
+f = list(range(10)) #f=range(10)
+print(f)
+random.shuffle(f)
+print(f)
+
+slice = random.sample(f, 5)
+print(slice)
