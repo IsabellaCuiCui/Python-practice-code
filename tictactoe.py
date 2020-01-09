758779c3652aa640a42211c9bf1a86ba0943c07f
IsabellaCuiCui <40281980+IsabellaCuiCui@users.noreply.github.com>
2020-01-09 15:42:30 +0800

    Create tictactoe.py

---
+++ tictactoe.py
@@ -0,0 +1,22 @@
+"""
+建立一个超简易版井字棋游戏
+"""
+#初始化九宫格名称
+theboard = {'top-L':' ', 'top-M':' ', 'top-R':' ',
+		'mid-L':' ', 'mid-M':' ', 'mid-R':' ',
+		'low-L':' ', 'low-M':' ', 'low-R':' '}
+
+#初始化井字棋盘
+def printBoard(board):
+	print('\t' + board['top-L'] + '|' + board['top-M'] + '|' + board['top-R']) #加t的原因是不希望井字格显示太靠边
+	print('\t' + '-+-+-')
+	print('\t' + board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+	print('\t' + '-+-+-')
+	print('\t' + board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+  
+#正式开始游戏
+theboard = {'top-L':'O', 'top-M':' ', 'top-R':' ',
+		'mid-L':' ', 'mid-M':' ', 'mid-R':' ',
+		'low-L':' ', 'low-M':'X', 'low-R':' '} 
+
+printBoard(theboard)
